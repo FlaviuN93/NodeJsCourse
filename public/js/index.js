ad65c6fa56bab00644bd3a59a79d1d01b80271cd
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-03-07 17:24:17 +0200

    Updated project

--- public/js/index.js
+++ public/js/index.js
@@ -67,4 +67,4 @@ if (bookBtn)
   });
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('success', alertMessage, 5);
+if (alertMessage) showAlert('success', alertMessage);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,7 +9,6 @@ const {
   alerts,
 } = require('../controllers/viewsController');
 const { isLoggedIn, protect } = require('../controllers/authController');
-const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
