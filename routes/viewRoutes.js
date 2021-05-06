b93498fc12ed91bafd4603ac2f988fd2644d2965
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-05-06 18:03:37 +0300

    Update

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -17,7 +17,7 @@ router.use(alerts);
 
 router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
-router.get('/signup', protect, getSignUpForm);
+router.get('/signup', getSignUpForm);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
 router.get('/my-tours', protect, getMyTours);
