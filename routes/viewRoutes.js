0aed6e5e27985178a662c39f61ec5e90b35e13b2
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-05-06 17:52:22 +0300

    Update

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -15,10 +15,10 @@ const router = express.Router();
 
 router.use(alerts);
 
+router.get('/signup', getSignUpForm);
 router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
-router.get('/signup', getSignUpForm);
 router.get('/me', protect, getAccount);
 router.get('/my-tours', protect, getMyTours);
 
