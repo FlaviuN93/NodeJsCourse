37181fa3a82ed6e2d29c2d28d70c621c040381b2
Flaviu Nemes <nemes.flaviu@gmail.com>
2021-05-06 18:28:21 +0300

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,8 +44,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const url = `${req.protocol}://${req.get('host')}/me`;
-  const mail = await new Email(newUser, url).sendWelcome();
+  // const url = `${req.protocol}://${req.get('host')}/me`;
+  // const mail = await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, req, res);
 });
